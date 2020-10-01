b12b5195962b5b640fe2c1bfbfac8b09a0b2426a
SabeloCodes <sabelocodes@gmail.com>
2020-10-01 20:24:20 +0000

    Created stripe elements.js

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HOkgXGECXxRDajteUG2vHYYrB0cMCHqUTeWvBIFYPzfoS7Yv9fglBXqGkkA1q2S3kS4b8ht8nyCQynPAk9wMFK200vBommBPk',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
